06638a11e0e68a317d8ddeda4362155dda058ebc
armando <jarodriguezvera@gmail.com>
2024-03-01 12:13:11 -0600

    Agregar archivos de proyecto.

---
+++ Arreglo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Arreglos_G2_2024_II
+{
+    internal class Arreglo
+    {
+        public int[] arregloInt;
+
+
+        public Arreglo(int N)
+        {
+            arregloInt = new int[N];
+        }
+        /// <summary>
+        /// Método estático que no depende del constructor
+        /// </summary>
+        public static void Imprime()
+        {
+            //   string [] arreglo = { "a", "b", "c", "d" };
+            int[] arreglo = { 20, 1, -10, 9, 5, 100, 3 };
+            Console.WriteLine("Imprimiendo de manera original");
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(" {0} ", arreglo[i]);
+
+            }
+            Array.Sort(arreglo);
+            //Imprimiendo de manera Ordenada
+            
+            Console.WriteLine("\n Imprime de manea Ordenada");
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(" {0} ", arreglo[i]);
+
+            }
+
+        }
+        public void Leer()
+        {
+            Console.WriteLine("Ingresa los elementos del Arreglo");
+            for( int i =0; i < arregloInt.Length; i++)
+            {
+                Console.WriteLine("Ingresa elemento Arreglo[{0}] = ", i);
+                arregloInt[i] = int.Parse( Console.ReadLine());
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Arreglos_G2_2024_II
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /* Ejemplo ocupando Constructor
+             * var miArreglo = new Arreglo(5);
+             miArreglo.Leer();
+             miArreglo.Imprime();*/
+
+            //Ejemplo ocupando métodos estáticos
+            Arreglo.Imprime();
+            Console.ReadLine();
+
+        }
+    }
+}
